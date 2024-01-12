0b02cc39e7c79b6f3eb879c9f33a023fc121e3af
Domingos Canhanga <domingos.canhanga@mamboo.co.ao>
2024-01-12 16:44:06 +0100

    feat: create migrations of notes links and tags

--- src/database/knex/migrations/20240112144909_create_notes.ts
+++ src/database/knex/migrations/20240112144909_create_notes.ts
@@ -1,12 +1,11 @@
 import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.createTable('users', table => {
+  await knex.schema.createTable('notes', table => {
     table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
-    table.string('name').notNullable();
-    table.string('email').notNullable();
-    table.string('password_hash').notNullable();
-    table.string('avatar');
+    table.string('title').notNullable();
+    table.string('description').notNullable();
+    table.uuid('user_id').references('id').inTable('users').onDelete('CASCADE');
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(null);
   });
--- src/modules/accounts/useCases/createUsers.useCase.ts
+++ src/modules/accounts/useCases/createUsers.useCase.ts
@@ -1,5 +1,4 @@
 import { hash } from 'bcrypt';
-import { randomUUID } from 'crypto';
 import { inject, injectable } from 'tsyringe';
 
 import { type ICreateUsersDTO } from '../dtos';
@@ -11,16 +10,21 @@ import { AppError } from '@/shared/errors/appError';
 
 @injectable()
 class CreateUsersUseCase {
-  constructor(@inject('UsersRepository') private readonly usersRepository: IUsersRepository) {}
+  constructor(
+    @inject('UsersRepository')
+    private readonly usersRepository: IUsersRepository
+  ) {}
+
   execute = async (data: ICreateUsersDTO): Promise<void> => {
-    const emailAlreadyExits = await this.usersRepository.findByEmail(data.email);
+    const emailAlreadyExits = await this.usersRepository.findByEmail(
+      data.email
+    );
     if (emailAlreadyExits) {
       throw new AppError('Esse email já está cadastrado', 409);
     }
 
     const password_hash = await hash(data.password, 6);
     const newUser: IUser = {
-      id: randomUUID(),
       name: data.name,
       email: data.email,
       password_hash
