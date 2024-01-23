56b279c801c50712e53ec4c21465a690573304f6
Domingos Canhanga <domingos.canhanga@mamboo.co.ao>
2024-01-23 16:13:11 +0100

    feat: crate controller to list the notes of user

--- src/modules/notes/http/routes/notes.routes.ts
+++ src/modules/notes/http/routes/notes.routes.ts
@@ -2,12 +2,17 @@ import { Router } from 'express';
 
 import { createNotesController } from '../controllers/createNotes.controllers';
 import { deleteNotesControllers } from '../controllers/deleteNotes.controllers';
+import { indexNotesController } from '../controllers/IndexNotes.controllers';
 import { listNotesController } from '../controllers/listNotes.controllers';
 
 import { ensureAuthenticated } from '@/middlewares/ensureAuthenticated';
 
 const notesRoutes = Router();
-notesRoutes.post('/', ensureAuthenticated, createNotesController.handle);
+
 notesRoutes.get('/:id', ensureAuthenticated, listNotesController.handle);
+notesRoutes.get('/', ensureAuthenticated, indexNotesController.handle);
+
+notesRoutes.post('/', ensureAuthenticated, createNotesController.handle);
+
 notesRoutes.delete('/:id', ensureAuthenticated, deleteNotesControllers.handle);
 export { notesRoutes as notes };
