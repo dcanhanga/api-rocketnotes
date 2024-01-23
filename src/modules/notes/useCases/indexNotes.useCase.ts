c55b0d9b27cf313e2bdf5d4203938de3bdb27254
Domingos Canhanga <domingos.canhanga@mamboo.co.ao>
2024-01-23 15:48:56 +0100

    feat: create useCase to list the note

---
+++ src/modules/notes/useCases/indexNotes.useCase.ts
@@ -0,0 +1,14 @@
+import { inject, injectable } from 'tsyringe';
+
+import { type INotes } from '../model/notes';
+import { INotesRepository } from '../repositories/interfaces/INotesRepository';
+
+@injectable()
+class IndexNotesUseCase {
+  constructor(@inject('NotesRepositories') private readonly indexNotesUseCase: INotesRepository) {}
+  execute = async (userId: string): Promise<INotes[]> => {
+    const notes = await this.indexNotesUseCase.index(userId);
+    return notes;
+  };
+}
+export { IndexNotesUseCase };
